303926ad1ac41cdafcd290017408058ead8540e3
auto402749 <56351076+auto402749@users.noreply.github.com>
2024-02-19 21:26:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def sendline():
   data = requests.post(url, headers=headers, data=data)   # 使用 POST 方法
 
 
-schedule.every(2).minute.do(sendline)
+schedule.every(2).minutes.do(sendline)
 #每個星期日的13:30分執行任務(線上編譯器為GMT時間，台灣為GMT+8，故設定上要-8)
 #schedule.every().sunday.at("05:30").do(sendline)
 while True:
