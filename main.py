2738dfed5db040019194241923b8c7eeb1367a4f
auto402749 <56351076+auto402749@users.noreply.github.com>
2024-02-20 10:09:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def sendline():
 
   #Line Notify權杖設定
   url = 'https://notify-api.line.me/api/notify'
-  token = 'vvr0RmVaZBqfUHhOohASfxRBqcqNtmntFOhi1JYB3pZ'
+  token = 'hbJvV0cpvjdAF270DO9zil9sXwjIhUs0ddGcjv80FlR'
 
   headers = {
     'Authorization': 'Bearer ' + token    # 設定權杖
@@ -72,8 +72,8 @@ def sendline():
 
 #schedule.every(2).minutes.do(sendline)
 #每個星期日的13:30分執行任務(線上編譯器為GMT時間，台灣為GMT+8，故設定上要-8)
-#schedule.every().sunday.at("05:30").do(sendline)
-schedule.every().tuesday.at("02:00").do(sendline)
+schedule.every().sunday.at("05:30").do(sendline)
+
 
 while True:
   schedule.run_pending()
